5bcc8de5dc9a0c5b13f932abf8a4ab66d08f6d27
Claudia Chittim <cchittim10101@gmail.com>
2022-02-15 13:20:16 -0600

    test

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+// this creates a function named "fight"
+
+var playername = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  // fight();
