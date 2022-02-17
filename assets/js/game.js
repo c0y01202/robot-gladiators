1976e5262d6b2a4eb7251798c06f68392d010336
Claudia Chittim <cchittim10101@gmail.com>
2022-02-17 17:08:17 -0600

    conditions

--- assets/js/game.js
+++ assets/js/game.js
@@ -29,14 +29,12 @@ fight();
 enemyHealth = enemyHealth - playerAttack;
 
 // Log a resulting message to the console so we know that it worked.
-console.log(
-  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-);
+console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 
-// check enemy's health
-if (enemyHealth <= 0) {
-  window.alert(enemyName + " has died!");
-} 
-else {
-  window.alert(enemyName + " still has " + enemyHealth + " health left.");
-}
+  // check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
